8eea1a816a0e38b47f7103d248d9b6fe9ac60ab4
OnkelDunkel <rasbalnor@gmail.com>
2019-02-07 21:12:25 +0100

    succesdfully loads file

--- twitter_db.py
+++ twitter_db.py
@@ -7,14 +7,54 @@
 
 
 
-import requests, zipfile, io, pprint, pymongo
-from pymongo import MongoClient
+import requests, zipfile, io, pprint, pymongo, csv
 
 dowload_url = "http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip"
-print("downloading")
-r = requests.get(dowload_url, stream=True)
-print("downloaded")
-z = zipfile.ZipFile(io.BytesIO(r.content))
-print("extracting")
-z.extractall()
-print("done")
+file_name = "testdata.manual.2009.06.14.csv"
+
+
+myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+db = myclient["twitterdb"]
+db2 = db["twitterdb"]
+db2.insert_one({"bob":"bob"})
+print("listing dbs")
+print(myclient.list_database_names())
+
+
+
+def retrieve_file():
+    try:
+        f = open(file_name)
+        f.close()
+    except FileNotFoundError:
+        print("-downloading twitter data")
+        r = requests.get(dowload_url, stream=True)
+        print("-creating zip file")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        print("-extracting zip file")
+        z.extractall()
+        print("-data was retrieved succesfully")
+
+def add_data_to_db():
+    csv.register_dialect("goo")
+    with open(file_name, encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            #pprint.pprint(row)
+            print(row[3])
+            if not len(row) == 6:
+                print(row)
+
+
+
+
+    '''
+    f = open(file_name, "r")
+    while f.
+    for i in range(0,10):
+    f.readline())
+    '''
+
+
+#retrieve_file()
+#add_data_to_db()
